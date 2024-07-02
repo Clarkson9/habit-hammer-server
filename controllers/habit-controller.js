28db959aed555e90085e28f69010887fb9a747d1
Alex <alexclarkson9@yahoo.com>
2024-07-02 14:52:32 -0400

    add preliminary code to return specified user habit list

--- controllers/habit-controller.js
+++ controllers/habit-controller.js
@@ -1,12 +1,18 @@
 const knex = require("knex")(require("../knexfile"));
 
-const getHabitList = async (_req, res) => {
+const getHabitList = async (req, res) => {
+	// Header id used to specify which user's habits to retrieve
+	const { id } = req.headers;
+
 	try {
-		// const data = await knex("user");
-		// res.status(200).json(data);
-		res.status(200).send("This is the GET /habit endpoint");
-	} catch (err) {
-		// res.status(400).send(`Error retrieving Users: ${err}`);
+		const habitList = await knex("habit")
+			.select("id", "habit_name", "habit_why", "streak", "progress")
+			.where({ user_id: id });
+		return res.status(200).json(habitList);
+		// res.status(200).send("This is the GET /habit endpoint");
+	} catch (error) {
+		console.error("Error getting habit list:", error);
+		res.status(500).json({ error: "Internal server error" });
 	}
 };
 
