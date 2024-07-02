65d20766ae92158cec9c63855e37150c8eb6e8a0
Alex <alexclarkson9@yahoo.com>
2024-07-02 15:53:37 -0400

    add preliminary logic to POST /habit endpoint to add a new habit to the database

--- controllers/habit-controller.js
+++ controllers/habit-controller.js
@@ -12,17 +12,58 @@ const getHabitList = async (req, res) => {
 		// res.status(200).send("This is the GET /habit endpoint");
 	} catch (error) {
 		console.error("Error getting habit list:", error);
-		res.status(500).json({ error: "Internal server error" });
+		res.status(500).json({
+			message: `Unable to get habit list: ${error}`,
+		});
 	}
 };
 
 const addHabit = async (req, res) => {
+	// Header id used to identify a specific user
+	const { id } = req.headers;
+	const { habit_name, habit_why } = req.body;
+
+	// Verify id is present in req.headers
+	if (!id) {
+		return res
+			.status(400)
+			.send("Failed to create new habit. No user id found in request header.");
+	}
+
+	// Verify that all expected req.body keys are present (all fields are required)
+	const reqBodyKeys = Object.keys(req.body);
+
+	if (
+		!reqBodyKeys.includes("habit_name") ||
+		!reqBodyKeys.includes("habit_why")
+	) {
+		return res
+			.status(400)
+			.send("Failed to create new habit. All form fields are required.");
+	}
+
+	// ************ REVISIT THE ABOVE VALIDATION LATER ************
+
+	// Passed validation, create new habit
+	const newHabit = {
+		user_id: id,
+		habit_name,
+		habit_why,
+	};
+
 	try {
-		// const data = await knex("user");
-		// res.status(200).json(data);
-		res.status(200).send("This is the POST /habit endpoint");
-	} catch (err) {
-		// res.status(400).send(`Error retrieving Users: ${err}`);
+		const result = await knex("habit").insert(newHabit);
+
+		const newHabitId = result[0];
+		const createdHabit = await knex("habit").where({
+			id: newHabitId,
+		});
+
+		res.status(201).json(createdHabit);
+	} catch (error) {
+		res.status(500).json({
+			message: `Unable to create new warehouse: ${error}`,
+		});
 	}
 };
 
